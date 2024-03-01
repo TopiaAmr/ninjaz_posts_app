cd0bbbc3197c22ebb2fb33fae6300e78a3884dca
Amr Elsaaadny <topia.amr@icloud.com>
2024-03-01 03:28:40 +0200

    adding docs

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -1,9 +1,25 @@
+/// Contains the base URL and API key for making requests to the DummyAPI.io API.
 class ApiConstants {
+  /// Private constructor to prevent instantiation.
   ApiConstants._();
 
+  /// The base URL for all API requests.
+  ///
+  /// Currently set to 'https://dummyapi.io'.
   static const String baseUrl = 'https://dummyapi.io';
+
+  /// The API key for authenticating API requests.
+  ///
+  /// Currently set to '65e080d27fecd42961c1131e'.
   static const String apiKey = '65e080d27fecd42961c1131e';
 
+  /// Returns a URL for retrieving a list of posts.
+  ///
+  /// The [page] parameter specifies which page of results to retrieve.
   static String posts(int page) => '$baseUrl/data/v1/post?limit=20&page=$page';
-  static const String post = '$baseUrl/data/v1/post';
+
+  /// The URL for retrieving a single post.
+  ///
+  /// The ID of the post is a path parameter.
+  static const String post = '$baseUrl/data/v1/post/{id}';
 }
--- test/features/posts/data/datasrouce/posts_remote_datasource_test.dart
+++ test/features/posts/data/datasrouce/posts_remote_datasource_test.dart
@@ -9,21 +9,32 @@ import 'package:ninjaz_posts_app/features/posts/domain/entities/post.dart';
 
 import 'response_examples.dart';
 
+/// A mock HTTP client for testing purposes.
 class MockHTTPClient extends Mock implements DioConsumer {}
 
+/// Unit tests for the [PostsRemoteDatasource].
 void main() {
+  /// The mock HTTP client.
   late ApiConsumer apiConsumer;
+
+  /// The class under test.
   late BasePostsRemoteDatasource datasource;
 
+  /// Set up the mock HTTP client and the class under test before each test.
   setUp(() {
     apiConsumer = MockHTTPClient();
     datasource = PostsRemoteDatasource(apiConsumer);
   });
 
+  /// Initialize the service locator before all tests.
   setUpAll(() => ServiceLocator.init());
 
+  /// Tests for the [getPosts] method.
   group('get posts', () {
+    /// Tests that the method returns a list of posts.
     test('should return a list of posts', () async {
+      // When the HTTP client is asked to get a list of posts,
+      // return a response with the expected list of posts.
       when(() => apiConsumer.get(any())).thenAnswer(
         (_) async => Response<String>(
           data: listOfPosts,
@@ -31,11 +42,16 @@ void main() {
           requestOptions: RequestOptions(path: ''),
         ),
       );
+      // Get the list of posts from the datasource.
       final response = await datasource.getPosts(1);
+      // Verify that the response is a list of posts.
       expect(response, isA<List<Post>>());
     });
 
+    /// Tests that the method returns an empty list of posts.
     test('should return an empty list of posts', () async {
+      // When the HTTP client is asked to get a list of posts,
+      // return a response with an empty list of posts.
       when(() => apiConsumer.get(any())).thenAnswer(
         (_) async => Response<String>(
           data: emptyResponse,
@@ -43,13 +59,19 @@ void main() {
           requestOptions: RequestOptions(path: ''),
         ),
       );
+      // Get the list of posts from the datasource.
       final response = await datasource.getPosts(1);
+      // Verify that the response is an empty list of posts.
       expect(response, isA<List<Post>>());
     });
   });
 
+  /// Tests for the [getPost] method.
   group('get single post', () {
+    /// Tests that the method returns a single post.
     test('should return a single post ', () async {
+      // When the HTTP client is asked to get a single post,
+      // return a response with the expected single post.
       when(() => apiConsumer.get(any())).thenAnswer(
         (_) async => Response<String>(
           data: singlePost,
@@ -57,11 +79,16 @@ void main() {
           requestOptions: RequestOptions(path: ''),
         ),
       );
+      // Get the single post from the datasource.
       final response = await datasource.getPost('1');
+      // Verify that the response is a single post.
       expect(response, isA<Post>());
     });
 
+    /// Tests that the method returns an error when the post is not found.
     test('should return an error when the post is not found', () async {
+      // When the HTTP client is asked to get a single post,
+      // return a response with a 404 status code.
       when(() => apiConsumer.get(any())).thenAnswer(
         (_) async => Response<String>(
           data: errorResponse,
@@ -69,7 +96,9 @@ void main() {
           requestOptions: RequestOptions(path: ''),
         ),
       );
+      // Get the single post from the datasource.
       final response = datasource.getPost('1');
+      // Verify that the response throws an error.
       expect(response, throwsException);
     });
   });
