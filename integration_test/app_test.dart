5b13749fb8e3208e744d19a32e86ea54cfa3ec65
Amr Elsaadny <topia.amr@icloud.com>
2024-03-02 20:07:49 +0200

    Update app_test.dart
    Adding documentation

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -9,7 +9,9 @@ import 'package:ninjaz_posts_app/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
+  
+  /// Tests the main functionality of the app, including interactions with
+  /// the bottom navigation bar, switching between tabs, and scrolling the posts list.
   testWidgets(
     'End to end testing',
     (tester) async {
@@ -21,6 +23,8 @@ void main() {
       expect(find.byType(ListOfPostsScreen), findsOneWidget);
       expect(find.byType(Tab2Screen), findsNothing);
       expect(find.byType(Tab3Screen), findsNothing);
+
+      /// Taps the "Tab 2" button on the bottom navigation bar.
       await tester.tap(find.text('Tab 2'));
       await tester.pumpAndSettle();
       expect(find.byType(BottomNavigation), findsOneWidget);
@@ -28,6 +32,8 @@ void main() {
       expect(find.byType(Tab2Screen), findsOneWidget);
       expect(find.byType(Tab3Screen), findsNothing);
       expect(find.text('Coming Soon'), findsOneWidget);
+      
+      /// Taps the "Tab 3" button on the bottom navigation bar.
       await tester.tap(find.text('Tab 3'));
       await tester.pumpAndSettle();
       expect(find.byType(BottomNavigation), findsOneWidget);
@@ -35,8 +41,13 @@ void main() {
       expect(find.byType(Tab2Screen), findsNothing);
       expect(find.byType(Tab3Screen), findsOneWidget);
       expect(find.text('Coming Soon'), findsOneWidget);
+      
+      /// Taps the "Posts" button on the bottom navigation bar.
       await tester.tap(find.text('Posts'));
       await tester.pumpAndSettle();
+
+      /// Simulates a swipe down gesture to scroll the posts list.
+      /// Finds multiple 'PostItemWidget' instances, verifying that posts are loaded and displayed correctly.
       final gesture = await tester.startGesture(Offset(0, 300));
       await gesture.moveBy(Offset(0, -300));
       await tester.pump();
